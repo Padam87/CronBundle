04c6e999b4cac7288b23d00150a532a018b87ab5
Adam Prager <prager.adam87@gmail.com>
2019-03-12 17:36:19 +0100

    Revert "Fixed command loading"
    This reverts commit 05f3f9006e14935cfcd6a8b560754990f7dd683c.

--- DependencyInjection/Padam87CronExtension.php
+++ DependencyInjection/Padam87CronExtension.php
@@ -3,8 +3,7 @@
 namespace Padam87\CronBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class Padam87CronExtension extends Extension
@@ -16,9 +15,6 @@ class Padam87CronExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yaml');
 
         $container->setParameter('padam87_cron', $config);
     }
