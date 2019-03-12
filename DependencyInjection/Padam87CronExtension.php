61a469b63b3c726098683e7f1073c0a62d676991
Adam Prager <prager.adam87@gmail.com>
2019-03-12 17:37:26 +0100

    Fixed command loading

--- DependencyInjection/Padam87CronExtension.php
+++ DependencyInjection/Padam87CronExtension.php
@@ -3,7 +3,8 @@
 namespace Padam87\CronBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class Padam87CronExtension extends Extension
@@ -15,6 +16,9 @@ class Padam87CronExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
         $container->setParameter('padam87_cron', $config);
     }
