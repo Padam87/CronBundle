d89538d370e2893f187fb26c6d9a0ab82a315264
Lennart <58809801+dragondungeon1@users.noreply.github.com>
2022-07-26 18:28:59 +0200

    feat: php 8 attributes added (#14)
    * feat: php 8 attributes added
    
    * feat: PR feedback, final commit
    
    * Update composer.json

--- Annotation/Job.php
+++ Annotation/Job.php
@@ -2,49 +2,42 @@
 
 namespace Padam87\CronBundle\Annotation;
 
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
+
 /**
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target("CLASS")
  */
+#[\Attribute(\Attribute::TARGET_CLASS)]
 class Job
 {
-    /**
-     * @var string
-     */
-    public $minute = '*';
+    public string $minute;
+    public string $hour;
+    public string $day ;
+    public string $month;
+    public string $dayOfWeek;
+    public ?string $group = null;
+    public ?string $logFile = null;
+    public ?string $commandLine = null;
 
-    /**
-     * @var string
-     */
-    public $hour = '*';
-
-    /**
-     * @var string
-     */
-    public $day = '*';
-
-    /**
-     * @var string
-     */
-    public $month = '*';
-
-    /**
-     * @var string
-     */
-    public $dayOfWeek = '*';
-
-    /**
-     * @var string
-     */
-    public $commandLine;
-
-    /**
-     * @var string
-     */
-    public $group;
-
-    /**
-     * @var string
-     */
-    public $logFile;
+    public function __construct(
+        string $minute = '*',
+        string $hour = '*',
+        string $day = '*',
+        string $month = '*',
+        string $dayOfWeek = '*',
+        ?string $group = null,
+        ?string $logFile = null,
+        ?string $commandLine = null
+    ) {
+        $this->minute = $minute;
+        $this->hour = $hour;
+        $this->day = $day;
+        $this->month = $month;
+        $this->dayOfWeek = $dayOfWeek;
+        $this->group = $group;
+        $this->logFile = $logFile;
+        $this->commandLine = $commandLine;
+    }
 }
