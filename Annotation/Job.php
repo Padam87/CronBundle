7e735223cc7ba5354650a50c5549d90ffa9032da
dragondungeon1 <lennartderidder@gmail.com>
2022-07-26 09:00:32 +0200

    feat: PR feedback, final commit

--- Annotation/Job.php
+++ Annotation/Job.php
@@ -2,54 +2,42 @@
 
 namespace Padam87\CronBundle\Annotation;
 
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
+
 /**
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target("CLASS")
  */
 #[\Attribute(\Attribute::TARGET_CLASS)]
 class Job
 {
-    public string $minute = '*';
-    public string $hour = '*';
-    public string $day = '*';
-    public string $month = '*';
-    public string $dayOfWeek = '*';
-
+    public string $minute;
+    public string $hour;
+    public string $day ;
+    public string $month;
+    public string $dayOfWeek;
     public ?string $group = null;
-
     public ?string $logFile = null;
-
     public ?string $commandLine = null;
 
     public function __construct(
-        string|array $minute = '*',
+        string $minute = '*',
         string $hour = '*',
         string $day = '*',
         string $month = '*',
         string $dayOfWeek = '*',
-        string $group = '',
-        string $logFile = '',
+        ?string $group = null,
+        ?string $logFile = null,
         ?string $commandLine = null
     ) {
-        if (is_array($minute)) {
-            // Invocation through annotations with an array parameter only
-            $this->processAttribute($minute);
-        } else {
-            $this->commandLine = $commandLine;
-            $this->minute = $minute;
-            $this->hour = $hour;
-            $this->day = $day;
-            $this->month = $month;
-            $this->dayOfWeek = $dayOfWeek;
-            $this->group = $group;
-            $this->logFile = $logFile;
-        }
-    }
-
-    public function processAttribute(array $attributes): void
-    {
-        foreach ($attributes as $name => $value) {
-            $this->$name = $value;
-        }
+        $this->minute = $minute;
+        $this->hour = $hour;
+        $this->day = $day;
+        $this->month = $month;
+        $this->dayOfWeek = $dayOfWeek;
+        $this->group = $group;
+        $this->logFile = $logFile;
+        $this->commandLine = $commandLine;
     }
 }
