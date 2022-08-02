fead5548f5d1747bb2d46ad1ee78606197a31a22
dragondungeon1 <lennartderidder@gmail.com>
2022-08-02 15:43:30 +0200

    fix: improvement

--- Util/Helper.php
+++ Util/Helper.php
@@ -36,6 +36,7 @@ class Helper
 
                 if (count($attributes) > 0) {
                     foreach ($attributes as $attribute) {
+                        $this->processJob(new Job(...$attribute->getArguments()), $input, $config, $commandInstance, $tab);
                         $this->processJob($attribute->newInstance(), $input, $config, $commandInstance, $tab);
                     }
 
